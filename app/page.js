22e7afce8916a8e92b79609e99733c68b6aac5b8
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-08-14 20:08:22 +0600

    remove useless codes

--- app/page.js
+++ app/page.js
@@ -1,19 +1,5 @@
-// import { checkIsAuthenticated } from "@/lib/auth/checkIsAuthenticated";
-// import { redirect } from "next/navigation";
 
 export default function Home() {
-  // const isAuthenticated = await checkIsAuthenticated();
-
-  // if (isAuthenticated) {
-  //   return (
-  //     <main>
-  //       Home Page
-  //       <ClientButton text="Sign Out"/>
-  //     </main>
-  //   )
-  // } else {
-  //   redirect("/auth/sign-in");
-  // }
   return (
     <main>
       Home Page
