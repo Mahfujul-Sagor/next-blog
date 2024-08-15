570bf932bb6fcf5a262071cea774a5d008d480cc
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-08-15 22:57:35 +0600

    add credential signup todo:credential signin, add env.example

--- components/GithubSigning.js
+++ components/GithubSigning.js
@@ -5,15 +5,15 @@ import { handleGithubSignIn } from '@/lib/auth/handleGithubSignIn';
 import { FaGithub } from "react-icons/fa";
 
 
-const GithubSignIn = () => {
+const GithubSigning = ({text}) => {
   return (
     <div className='border rounded-lg flex items-center justify-center'>
-      <button onClick={()=> handleGithubSignIn()} className='flex gap-2 items-center justify-center w-full h-full py-4 bg-white hover:bg-stone-50 rounded-lg'>
+      <button onClick={()=> handleGithubSignIn()} className='flex gap-2 items-center justify-center w-full h-full py-4 bg-white lg:hover:bg-stone-50 rounded-lg'>
         <FaGithub className='text-2xl'/>
-        Sign in with Google
+        <span className=''>Sign {text} with Github</span>
       </button>
     </div>
   )
 }
 
-export default GithubSignIn;
+export default GithubSigning;
