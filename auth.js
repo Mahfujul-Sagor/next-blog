751a68ce7cd9aa89ed25260d98f686d221474662
Mahfujul-Sagor <mahfujulsagor@gmail.com>
2024-08-15 17:42:34 +0600

    add github oauth provider

--- auth.js
+++ auth.js
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "./db/connect";
 import Google from "next-auth/providers/google";
+import Github from "next-auth/providers/github";
  
 export const { handlers, signIn, signOut, auth } = NextAuth({
   trustHost: true,
